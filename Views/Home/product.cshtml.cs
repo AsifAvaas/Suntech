268a892895fd62700656eb1e1292bd8258dd2b02
tanjir238 <tanjir238@gmail.com>
2025-09-09 03:24:16 +0600

    Product Page

---
+++ Views/Home/product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SunTech.Views.Home
+{
+    public class productModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
