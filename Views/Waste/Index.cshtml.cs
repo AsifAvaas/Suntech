8b84d78e15a8d68a1bd1ac44f95502157998a5b1
Elman7 <elmannayeem1@gmail.com>
2025-09-15 02:20:02 +0600

    waste section is working

---
+++ Views/Waste/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SunTech.Views.Waste
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
